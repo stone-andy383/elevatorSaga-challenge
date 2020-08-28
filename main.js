217b8b225132bbaeff196cc8c54dadf13cf87fcc
stone-andy383 <Andy@Leanas-MacBook-Pro-2.local>
2020-08-27 21:58:35 -0500

    Challenge 1 - Modify elevator idle event to include floor 2

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 {
   /*
-   * Elevator Saga code challenge
-   * https://play.elevatorsaga.com/
-   */
+ * Elevator Saga code challenge
+ * https://play.elevatorsaga.com/
+ */
 
   init: function(elevators, floors) {
       var elevator = elevators[0]; // Let's use the first elevator
 
       // Whenever the elevator is idle (has no more queued destinations) ...
       elevator.on("idle", function() {
-          // let's go to all the floors (or did we forget one?)
+          // let's go to all the floors
           elevator.goToFloor(0);
           elevator.goToFloor(1);
+          elevator.goToFloor(2);
       });
   },
-  update: function(dt, elevators, floors) {
-      // We normally don't need to do anything here
-  }
+      update: function(dt, elevators, floors) {
+          // We normally don't need to do anything here
+      }
 }
