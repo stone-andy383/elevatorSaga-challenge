40de97b0f51bbf9179480b4ea8e2e0a5ee02a2dd
stone-andy383 <Andy@Leanas-MacBook-Pro-2.local>
2020-08-30 11:06:57 -0500

    Challenge 2 - Add event listener per floor to add floor to queue.  When button pressed inside of elevator, add floor to queue

--- main.js
+++ main.js
@@ -3,16 +3,34 @@
  * Elevator Saga code challenge
  * https://play.elevatorsaga.com/
  */
-
+  
   init: function(elevators, floors) {
-      var elevator = elevators[0]; // Let's use the first elevator
+      // Elevator variable
+      var elevator = elevators[0];
+      
+      // Create loop to go through every floor and create a event listener for button pushes
+      for (i = 0; i < floors.length; i++) {
+          let floor = floors[i];
+          
+          // Add floor to queue if up button is pressed
+          floor.on("up_button_pressed", function(floor) {
+              elevator.goToFloor(floor.floorNum());
+          });
+          
+          // Add floor to queue if down button is pressed
+          floor.on("down_button_pressed", function(floor) {
+              elevator.goToFloor(floor.floorNum());
+          });
+      }
 
       // Whenever the elevator is idle (has no more queued destinations) ...
       elevator.on("idle", function() {
-          // let's go to all the floors
-          elevator.goToFloor(0);
-          elevator.goToFloor(1);
-          elevator.goToFloor(2);
+          
+      });
+
+      // add floor event lisenter to add floor to queue when guest presses a button
+      elevator.on("floor_button_pressed", function(floorNum) {
+          elevator.goToFloor(floorNum)
       });
   },
       update: function(dt, elevators, floors) {
