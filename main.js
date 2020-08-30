872a285feae7c4e9f9ae283f606d404b6f5ea1af
stone-andy383 <Andy@Leanas-MacBook-Pro-2.local>
2020-08-30 11:27:50 -0500

    Challenge 3 - Add logic to remove extra calls to the same floor if the elevator has already fullfilled the calls to that floor

--- main.js
+++ main.js
@@ -25,13 +25,31 @@
 
       // Whenever the elevator is idle (has no more queued destinations) ...
       elevator.on("idle", function() {
-          
+
       });
 
-      // add floor event lisenter to add floor to queue when guest presses a button
+      // Add floor event lisenter to add floor to queue when guest presses a button
       elevator.on("floor_button_pressed", function(floorNum) {
           elevator.goToFloor(floorNum)
       });
+      
+      // Add event listener for when the elevator is stopped at a floor
+      elevator.on("stopped_at_floor", function(floorNum) {
+          // Grab load factor from elevator
+          let elevatorEmpty = elevator.loadFactor();
+          
+          // Check if elevator is full. If it's not full, remove all additional calls to this floor from queue
+          // If last person entered made elevator full, this will leave an extra call to the floor still
+          if (elevatorEmpty < 1) {
+              for(let i = 0; i < elevator.destinationQueue.length; i++) {
+                  if (elevator.destinationQueue[i] === floorNum) {
+                      elevator.destinationQueue.splice(i, 1);
+                  }
+              }
+              // Update queue once floors removed
+              elevator.checkDestinationQueue();
+          }
+      })
   },
       update: function(dt, elevators, floors) {
           // We normally don't need to do anything here
